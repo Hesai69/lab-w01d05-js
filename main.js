6c6924c48679ea00f003d705d673726a941ae383
Qian Chen <oda_san@hotmail.com>
2016-12-04 18:13:41 -0800

    complete no style

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 console.log('JS linked');
 
 var input = document.querySelector('.new-todo');
+
 var handleInput = function(event){
+  // runs after user presses Enter key
   if(event.keyCode===13){
     // saves entered input
     var value = event.target.value;
@@ -11,19 +13,50 @@ var handleInput = function(event){
     var newListItem = document.createElement('li');
     // create span
     var newSpan = document.createElement('span');
+    // create remove option
+    var removeLi = null;
+
     // sets list item content to user input
     newListItem.textContent = value;
     // set list item class to 'todo'
     newListItem.setAttribute('class', 'todo');
     // add text content of 'x'
-    newSpan.textContent = 'x';
+    newSpan.textContent = ' x';
     // set span class to 'remove'
     newSpan.setAttribute('class', 'remove');
-    // adds list item to the list
+    newSpan.style.setProperty('text-decoration', 'none');
+    // adds remove to listItem and listItem to the list
     newListItem.appendChild(newSpan);
     list.appendChild(newListItem);
     // resets input field
     input.value = '';
+
+    // add listener to remove option
+    newSpan.addEventListener('click', removeItem);
+    // add event listener to line through list item
+    newListItem.addEventListener('click', isComplete);
+  }
+}
+var removeItem = function(event){
+  // remove clicked listItem from the list
+  this.parentNode.remove();
+}
+var isComplete = function(event){
+  console.log('this=' + this);
+  // add complete status
+  if(this.getAttribute('class')==='todo'){
+    // add class complete to listItem
+    this.setAttribute('class', 'todo complete');
+    console.log('line through');
+    // put line through list item
+    this.style.setProperty('text-decoration', 'line-through');
+  } // remove complete status
+  else if(this.getAttribute('class')==='todo complete'){
+    // removing class complete from listItem
+    this.setAttribute('class', 'todo');
+    console.log('remove line through');
+    // remove line through list item
+    this.style.setProperty('text-decoration', 'none');
   }
 }
 input.addEventListener('keyup', handleInput);
